b075fd326e5f12b0d0270e28c6728612ccc84b39
Rolando Ruiz <Rolando Ruiz>
2016-10-08 21:39:28 -0500

    Control por color añadido. Mejora en el agarre

---
+++ Assets/Scripts/ColorPanel.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var colorKey:GameObject;
+var colorSet = 0;
+
+
+function Start () {
+	
+}
+
+function Update () {
+
+	switch (colorSet){
+		case 1:
+			colorKey.GetComponent.<Renderer>().material.color = Color.red;
+		break;
+
+		case 2:
+			colorKey.GetComponent.<Renderer>().material.color = Color.green;
+		break;
+
+		case 3:
+			colorKey.GetComponent.<Renderer>().material.color = Color.blue;
+		break;
+
+		case 4:
+			colorKey.GetComponent.<Renderer>().material.color = Color.yellow;
+		break;
+	}
+}
--- Assets/Scripts/PlayableCharacter.js
+++ Assets/Scripts/PlayableCharacter.js
@@ -4,6 +4,8 @@ var mainCharacter:GameObject;
 var camara:GameObject;
 var collidingObject:GameObject;
 
+var movement:float;
+
 var colliding = false;		//maneja los eventos de collisiones para mantener exclusividad mutua con otras actividades
 var grab = false;			//controla los agarres
 
@@ -22,22 +24,25 @@ function Start () {
 }
 
 function Update () {
+
 	if(Input.GetKey(KeyCode.D)){	//movimiento a la derecha
-		speedx +=0.05;
+		speedx += 0.5;
 	}
 
 	if(Input.GetKey(KeyCode.A)){	//movimiento a la izquierda
-		speedx -=0.05;
+		speedx -= 0.5;
 	}
 	if(Input.GetKey(KeyCode.W)){	//movimiento hacia arriba
-		speedy +=0.05;
+		speedy += 0.5;
 	}
 	if(Input.GetKey(KeyCode.S)){	//movimiento hacia abajo
-		speedy -=0.05;
+		speedy -= 0.5;
 	}
 
+	//======================AGARRE====================================
+
 	//solo si esta cerca de algun objeto movible se efectua la acción de agarrar
-	if(Input.GetKeyUp(KeyCode.Space) && colliding){
+	if(Input.GetKeyUp(KeyCode.Space) && colliding && collidingObject.tag == "Mobile"){
 		grab = true;
 		collidingObject.GetComponent.<Renderer>().material.color = Color.red;
 	}
@@ -65,6 +70,18 @@ function Update () {
 		collidingObject.GetComponent.<Renderer>().material.color = Color.white;
 	}
 
+	//===================CODIGO DE COLORES==============================
+
+	if(Input.GetKeyDown(KeyCode.Space) && colliding && collidingObject.tag == "ColorCode"){
+		var script: ColorPanel = collidingObject.GetComponent(ColorPanel);
+		if(script.colorSet > 3){
+			script.colorSet = 0;
+		}else{
+			script.colorSet += 1;
+		}
+	}
+
+
 	//se altera la velocidad del jugador
 	mainCharacter.GetComponent.<Rigidbody>().velocity = new Vector3(speedx, speedy, 0);
 
@@ -75,8 +92,8 @@ function Update () {
 function OnCollisionEnter(col:Collision){
 	colliding = true;
 
-	if(col.gameObject.name == "Puzzle1"){
-		collidingObject = GameObject.Find("Puzzle1");
+	if(col.gameObject.tag == "Mobile"){
+		collidingObject = col.gameObject;
 
 		//para amantener el objeto agarrado relativo a la posicion del jugador se toman estos valores
 		if(collidingObject.transform.position.x < 0 || mainCharacter.transform.position.x < 0){
@@ -86,6 +103,10 @@ function OnCollisionEnter(col:Collision){
 		}
 		//disty = collidingObject.transform.position.y - mainCharacter.transform.position.y;
 		//distz = collidingObject.transform.position.z - mainCharacter.transform.position.z;
+	}
+	if(col.gameObject.tag == "ColorCode"){
+		collidingObject = col.gameObject;
+		colliding = true;
 	}
 }
 
